a5b77d2a8f83cd36d0f4813a1de2d24fc0e454af
Reem <reemmohamed012020@gmail.com>
2026-02-04 15:18:58 +0200

    Add Product Details routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProductDetails } from './features/Product Details/product-details/product-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'product/:id', component: ProductDetails }
+];
