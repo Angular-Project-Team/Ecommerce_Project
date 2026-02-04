66e2fea0dfe81e6f073d862d73375ae2beaad76c
Rodan <mohamedrody554@gmail.com>
2026-02-04 12:18:19 +0200

    adding Home components with servicies and modules and database.json

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
 import { Component, signal } from '@angular/core';
 
+import { HeroSection } from './features/components/hero-section/hero-section';
+import { Header } from './shared/components/header/header';
+import { Home } from './features/components/home/home';
+import { Products } from './features/components/products/products';
+
+
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Products,Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
 }
