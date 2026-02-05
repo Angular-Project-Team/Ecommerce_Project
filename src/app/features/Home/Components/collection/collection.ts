9664ab9787233f286f1e9b10b308ece5047c149f
Rodan <mohamedrody554@gmail.com>
2026-02-05 18:44:35 +0200

    feat: update Home components with new layout and styles, enhance product display

--- src/app/features/Home/Components/collection/collection.ts
+++ src/app/features/Home/Components/collection/collection.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { ProductType } from '../../../../shared/models/productType';
+import { Product } from '../../../../shared/components/product/product';
+import { ProductService } from '../../../services/product-service';
 
 @Component({
   selector: 'app-collection',
-  imports: [],
+  imports: [Product],
   templateUrl: './collection.html',
   styleUrl: './collection.css',
 })
 export class Collection {
+Math = Math;
+constructor(private productServices:ProductService) {}
+  products=signal([] as ProductType[])
+  ngOnInit(){
+    this.productServices.allProducts().subscribe(({
+      next:(data)=>{
+        this.products.set(data);
+      },
+      error:(err)=>{console.log(err)}
 
+}))};
 }
