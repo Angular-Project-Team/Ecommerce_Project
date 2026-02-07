da63ccaf4c86c2bf2c3005a0a6cc59a44c4aa346
Reem <reemmohamed012020@gmail.com>
2026-02-07 17:02:09 +0200

    resolve merge conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,16 @@
-<<<<<<< HEAD
-import { Routes } from '@angular/router';
+import { Routes , RouterModule } from '@angular/router';
 import { ProductDetails } from './features/product-details/product-details';
 import { Home } from './features/Home/home';
-import { Products } from './features/Home/Components/products/products';
+import { Shopping } from './features/shopping/shopping';
+import { Favourite } from './features/favourite/favourite';
+import { BestSeller } from './features/Home/Components/best-seller/best-seller';
 export const routes: Routes = [
     // { path: '', redirectTo: 'home', pathMatch: 'full' },
     // { path: 'home', component: Home },
     // { path: 'products', component: Products },
-    { path: 'products/:id', component: ProductDetails }
-=======
-import { RouterModule, Routes } from '@angular/router';
-import { Home } from './features/Home/home';
-import { Shopping } from './features/shopping/shopping';
-import { BestSeller } from './features/Home/Components/best-seller/best-seller';
-import { Favourite } from './features/favourite/favourite';
-
-
-export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full' },
   {path:'home', component: Home},
   {path:'shopping', component: Shopping},
   {path:'favourite', component: Favourite},
-
-  // {path:'product/:id', component: ProductDetails},
-
-  // {path:'**',component:Error},
->>>>>>> origin/Home
+  { path: 'products/:id', component: ProductDetails }
 ];
