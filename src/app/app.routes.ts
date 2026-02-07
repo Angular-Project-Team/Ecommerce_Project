b85a68e4bdfb7965fbd8e7dbd0f8afea6cd58d65
Reem <reemmohamed012020@gmail.com>
2026-02-07 16:52:37 +0200

    update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { ProductDetails } from './features/product-details/product-details';
 import { Home } from './features/Home/home';
 import { Products } from './features/Home/Components/products/products';
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: Home },
-    { path: 'products', component: Products },
+    // { path: '', redirectTo: 'home', pathMatch: 'full' },
+    // { path: 'home', component: Home },
+    // { path: 'products', component: Products },
     { path: 'products/:id', component: ProductDetails }
 ];
